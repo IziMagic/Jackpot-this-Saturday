d0f94cafd67a1ec1967551a63608fbccb1143525
Daria <shirai.ryu.hanzo.hasashi@gmail.com>
2025-08-08 15:04:22 +0300

    animation upgrade

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+const text = "must be won";
+const container = document.getElementById("animated-text");
+
+[...text].forEach((char, i) => {
+	const span = document.createElement("span");
+	span.textContent = char;
+	span.style.animationDelay = `${i * 0.15}s`;
+	container.appendChild(span);
+});
+
+
+setTimeout(() => {
+	const btnText = "Play now";
+	const btnContainer = document.getElementById("animated-btn-text");
+
+	[...btnText].forEach((char, i) => {
+		const span = document.createElement("span");
+		span.textContent = char;
+		span.style.animationDelay = `${i * 0.15}s`;
+		btnContainer.appendChild(span);
+	});
+}, 4000);
+
